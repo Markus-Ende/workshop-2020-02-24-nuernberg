8159e33209ebcbfff33eb43b46857c8407c0ac6f
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-02-25 15:35:59 +0100

    routing

--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'books',
+  component: BookListComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
