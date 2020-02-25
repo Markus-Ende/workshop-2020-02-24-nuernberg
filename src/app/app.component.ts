c66ff57f509cbef7e5366f5770806d545f685560
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-02-25 07:28:56 +0100

    title-box 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'The Bookmonkey';
 
+  onTitleClick(e: string) {
+    console.log(`clicked: ${e}`);
+    this.title += '!';
+  }
 }
--- src/app/title-box/title-box.component.ts
+++ src/app/title-box/title-box.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'sft-title-box',
   templateUrl: './title-box.component.html',
   styleUrls: ['./title-box.component.scss']
 })
-export class TitleBoxComponent implements OnInit {
+export class TitleBoxComponent {
   @Input() title = '';
+  @Output() titleClick = new EventEmitter<string>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  emitTitleClick() {
+    this.titleClick.emit(this.title);
   }
 
 }
